81b2e1e3bb94b976e69df369e149e343d794af3c
AleksanderSkubala <alexskubala@gmail.com>
2020-01-13 22:37:47 +0100

    Turn on PWA. Add simple table

---
+++ src/components/DataTable.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Table = styled.table`
+border-collapse: collapse;
+
+th {
+    border: 1px solid black;
+    padding: 15px;
+    font-size: 18px;
+  }
+`;
+
+function DataTable() {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Symbol</th>
+          <th>Data</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>v=</th>
+          <th>
+            <input />
+          </th>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+export default DataTable;
---
+++ src/theme.js
@@ -0,0 +1,5 @@
+const colors = {
+  primary: 'black'
+}
+
+export default colors
