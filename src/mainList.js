c1f8718376c8c1931dde7941e26c1ae12d97b896
AleksanderSkubala <alexskubala@gmail.com>
2020-01-28 22:57:15 +0100

    Add formulas from kinemathics

--- src/mainList.js
+++ src/mainList.js
@@ -1,5 +1,19 @@
-export const mainList = [
+/*export const mainList = [
   {name: 'prędkość w ruchu po okręgu', formula: 'v=s/t', latex: "v= \\frac s t", category: "Kinematyka"},
   {name: 'prędkość w ruchu jedn.', formula: 'v=(s/t)*1.23', latex: `a^2 + b^2 = c^2`, category: "Kinematyka"},
   {name: 'prędkość Żyda', formula: 'v=h*v', latex: `V=h*v`, category: "happy math"},
+];*/
+
+export const mainList = [
+  {name: 'prędkość', formula: 'v=r/t', latex: "\\vec{v}=\\frac{\\Delta\\vec{r}}{\\Delta t}", category: "Kinematyka"},
+  {name: 'przyspieszenie', formula: 'a=v/t', latex: "\\vec{a}=\\frac{\\Delta\\vec{v}}{\\Delta t}", category: "Kinematyka"},
+  {name: 'prędkość kątowa (1)', formula: 'o=a/t', latex: "\\omega=\\frac{\\Delta \\alpha}{\\Delta t}", category: "Kinematyka"},
+  {name: 'prędkość kątowa (2)', formula: 'o=(2*3,1416)/t', latex: "\\omega=\\frac{2\\pi}{T}", category: "Kinematyka"},
+  {name: 'prędkość w ruchu po okręgu', formula: 'v=o*r', latex: "v=\\omega \\cdotp r", category: "Kinematyka"},
+  {name: 'przyspieszenie dośrodkowe (1)', formula: 'a=(v**2)/r', latex: "a_d=\\frac{v^2}{r}", category: "Kinematyka"},
+  {name: 'przyspieszenie dośrodkowe (2)', formula: 'a=(o**2)*r', latex: "a_d=\\omega^2 \\cdotp r", category: "Kinematyka"},
+  {name: 'przyspieszenie kątowe', formula: 'e=o/t', latex: "\\varepsilon=\\frac{\\Delta\\omega}{\\Delta t}", category: "Kinematyka"},
+  {name: 'przyspieszenie styczne', formula: 'a=e*r', latex: "a_{st}=\\varepsilon \\cdotp r", category: "Kinematyka"},
+  {name: 'prędkość w prostoliniowym ruchu jednostajnie zmiennym', formula: 'v=v+(a*t)', latex: "v=v_0 + a \\cdotp t", category: "Kinematyka"},
+  {name: 'droga w prostoliniowym ruchu jednostajnie zmiennym', formula: 's=(v*t)+((0.5*a)*(t**2))', latex: "s=v_0 \\cdotp t + \\frac{1}{2} a \\cdotp t^2", category: "Kinematyka"},
 ];
